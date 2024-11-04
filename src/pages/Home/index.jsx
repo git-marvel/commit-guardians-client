e2b492c4098b9865cd1316efb997a7cb540be7af
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2024-11-04 21:08:59 +0900

    feat: 입력된 form 정보에 따른 repository의 커밋목록 받아오는 기능 구현
    Organization name과 repository name을 받는 form을 생성 했습니다.
    버튼을 누르면 입력된 정보를 기반으로 해당 repository의 commit 목록을 받아와 평탄화 합니다.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,64 @@
+const githubToken = import.meta.env.VITE_GITHUB_TOKEN;
+const COMMITS_PER_PAGE = 100;
+
 const Home = () => {
-  return <h1>Init</h1>;
+  const handleRepoInfoSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const formJson = Object.fromEntries(formData.entries());
+    const [organizationName, repositoryName] = [
+      formJson.organizationName,
+      formJson.repositoryName,
+    ];
+
+    const gitcommitResponse = await fetch(
+      `https://api.github.com/repos/${organizationName}/${repositoryName}/commits?per_page=${COMMITS_PER_PAGE}&page=1`,
+      {
+        headers: {
+          Authorization: `token ${githubToken}`,
+        },
+      }
+    );
+    const gitcommitFirstPage = await gitcommitResponse.json();
+
+    const linkHeader = gitcommitResponse.headers.get("Link");
+    const lastPageNumber = linkHeader
+      ? parseInt(linkHeader.match(/&page=(\d+)>; rel="last"/)?.[1])
+      : 1;
+
+    const allCommits = gitcommitFirstPage;
+
+    if (lastPageNumber > 1) {
+      const fetchPromises = [];
+      for (let page = 2; page <= lastPageNumber; page++) {
+        fetchPromises.push(
+          fetch(
+            `https://api.github.com/repos/${organizationName}/${repositoryName}/commits?per_page=${COMMITS_PER_PAGE}&page=${page}`,
+            {
+              headers: {
+                Authorization: `token ${githubToken}`,
+              },
+            }
+          ).then((res) => res.json())
+        );
+      }
+
+      allCommits.push((await Promise.all(fetchPromises)).flat());
+    }
+  };
+
+  return (
+    <form method="post" onSubmit={handleRepoInfoSubmit}>
+      <label>
+        Organization name:{" "}
+        <input name="organizationName" defaultValue="ex) facebook" />
+        Repository name:{" "}
+        <input name="repositoryName" defaultValue="ex) react" />
+      </label>
+      <button type="submit">git api 요청</button>
+    </form>
+  );
 };
 
 export default Home;
