cd32f1dce0e244e8edc8b75bfc73515eceacd7e2
howyoujini <howyoujini@gmail.com>
2024-11-04 17:46:29 +0900

    feat: 정의하지 않은 url 이동시 notFoundPage 띄우기

---
+++ src/shared/components/Error.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+const Error = ({ code, heading, paragraph, button }) => {
+  return (
+    <main className="flex flex-col items-center">
+      <h1 className="font-Pixelify text-7xl">{code}</h1>
+      <h1 className="font-Pixelify text-2xl">{heading}</h1>
+      {paragraph && <p className="m-4">{paragraph}</p>}
+      {button}
+    </main>
+  );
+};
+
+Error.propTypes = {
+  code: PropTypes.string,
+  heading: PropTypes.string,
+  paragraph: PropTypes.string,
+  button: PropTypes.any,
+};
+
+export default Error;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      Pixelify: ["Pixelify"],
+    },
   },
   plugins: [],
 };
