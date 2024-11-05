592c00301f68af5ff0fbabd078440b01a9018e61
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2024-11-05 20:47:37 +0900

    refactor: 여러번 사용되는 URL을 상수로 선언

--- src/pages/Home/api/getCommitList.js
+++ src/pages/Home/api/getCommitList.js
@@ -6,36 +6,34 @@ const getCommitList = async ({
   repositoryName,
   setCommitList,
 }) => {
-  try {
-    const gitcommitResponse = await fetch(
-      `https://api.github.com/repos/${organizationName}/${repositoryName}/commits?per_page=${COMMITS_PER_PAGE}&page=1`,
-      {
-        headers: {
-          Authorization: `token ${githubToken}`,
-        },
-      }
-    );
-    const gitcommitFirstPage = await gitcommitResponse.json();
+  const commitListUrl = `https://api.github.com/repos/${organizationName}/${repositoryName}/commits?per_page=${COMMITS_PER_PAGE}`;
 
-    const linkHeader = gitcommitResponse.headers.get("Link");
+  const fetchCommits = async (baseUrl, page) => {
+    const gitCommitResponse = await fetch(`${baseUrl}&page=${page}`, {
+      headers: {
+        Authorization: `token ${githubToken}`,
+      },
+    });
+
+    return gitCommitResponse;
+  };
+
+  try {
+    const gitCommitResponse = await fetchCommits(commitListUrl, 1);
+    const gitCommitFirstPage = await gitCommitResponse.json();
+
+    const linkHeader = gitCommitResponse.headers.get("Link");
     const lastPageNumber = linkHeader
       ? parseInt(linkHeader.match(/&page=(\d+)>; rel="last"/)?.[1])
       : 1;
 
-    const allCommits = gitcommitFirstPage;
+    const allCommits = gitCommitFirstPage;
 
     if (lastPageNumber > 1) {
       const fetchPromises = [];
       for (let page = 2; page <= lastPageNumber; page++) {
         fetchPromises.push(
-          fetch(
-            `https://api.github.com/repos/${organizationName}/${repositoryName}/commits?per_page=${COMMITS_PER_PAGE}&page=${page}`,
-            {
-              headers: {
-                Authorization: `token ${githubToken}`,
-              },
-            }
-          ).then((res) => res.json())
+          fetchCommits(commitListUrl, page).then((res) => res.json())
         );
       }
 
@@ -43,6 +41,7 @@ const getCommitList = async ({
     }
 
     setCommitList(allCommits.flat());
+    console.log(allCommits);
   } catch (error) {
     throw new Error(error);
   }
