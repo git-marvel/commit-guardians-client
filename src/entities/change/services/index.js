fb735bc673ad5d28845c9fa81083131a5ffa1d39
soohan <evepledge@gmail.com>
2024-11-06 15:12:24 +0900

    refactor: API 공통 url 분리

--- src/entities/change/services/index.js
+++ src/entities/change/services/index.js
@@ -1,28 +1,6 @@
 import { createChangeEntity } from "../changeEntity";
 
-const VITE_GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 const NO_NEW_LINE_MESSAGE = "\\ No newline at end of file";
-
-const getDiff = async (owner, repo, ref) => {
-  const commitData = {
-    owner: owner,
-    repo: repo,
-    ref: ref,
-  };
-
-  const commitUrl = `https://api.github.com/repos/${commitData.owner}/${commitData.repo}/commits/${commitData.ref}`;
-
-  const response = await fetch(commitUrl, {
-    headers: {
-      Authorization: `token ${VITE_GITHUB_TOKEN}`,
-      Accept: "application/vnd.github.diff",
-    },
-  });
-
-  const changedCode = await response.text();
-
-  return getChanges(changedCode);
-};
 
 const getChanges = (diffPerCommit) => {
   const codeListPerFile = diffPerCommit.split("diff --git").slice(1);
@@ -66,4 +44,4 @@ const makeAChange = (commitCode) => {
   return { fileName: fileName, changes: changes };
 };
 
-export { getDiff, getChanges, makeAChange };
+export { getChanges, makeAChange };
--- src/pages/Home/api/getCommitList.js
+++ src/pages/Home/api/getCommitList.js
@@ -1,49 +1,77 @@
-const githubToken = import.meta.env.VITE_GITHUB_TOKEN;
+import { getChanges } from "../../../entities/change/services";
+import { GITHUB_TOKEN } from "../../../shared/constants/env";
+
 const COMMITS_PER_PAGE = 3;
+const DIFF_MEDIA_TYPE = "application/vnd.github.diff";
+
+const setCommitBaseUrl = ({ owner, repo }) =>
+  `https://api.github.com/repos/${owner}/${repo}/commits`;
+
+const fetchWithAuth = async (url, accept = "*/*") => {
+  return await fetch(url, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`,
+      Accept: accept,
+    },
+  });
+};
 
 const getCommitList = async ({
   organizationName,
   repositoryName,
   setCommitList,
 }) => {
-  const commitListUrl = `https://api.github.com/repos/${organizationName}/${repositoryName}/commits?per_page=${COMMITS_PER_PAGE}`;
-
-  const fetchCommits = async (baseUrl, page) => {
-    const gitCommitResponse = await fetch(`${baseUrl}&page=${page}`, {
-      headers: {
-        Authorization: `token ${githubToken}`,
-      },
-    });
-
-    return gitCommitResponse;
-  };
+  const commitListUrl = `${setCommitBaseUrl({ owner: organizationName, repo: repositoryName })}?per_page=${COMMITS_PER_PAGE}`;
 
   try {
-    const gitCommitResponse = await fetchCommits(commitListUrl, 1);
-    const gitCommitFirstPage = await gitCommitResponse.json();
-
+    const gitCommitResponse = await fetchWithAuth(`${commitListUrl}&page=${1}`);
     const linkHeader = gitCommitResponse.headers.get("Link");
     const lastPageNumber = linkHeader
       ? parseInt(linkHeader.match(/&page=(\d+)>; rel="last"/)?.[1])
       : 1;
 
+    const gitCommitFirstPage = await gitCommitResponse.json();
     const allCommits = gitCommitFirstPage;
 
     if (lastPageNumber > 1) {
       const fetchPromises = [];
+
       for (let page = 2; page <= lastPageNumber; page++) {
         fetchPromises.push(
-          fetchCommits(commitListUrl, page).then((res) => res.json())
+          fetchWithAuth(`${commitListUrl}&page=${page}`).then((res) =>
+            res.json()
+          )
         );
       }
 
       allCommits.push((await Promise.all(fetchPromises)).flat());
     }
 
-    setCommitList(allCommits.flat());
+    const filterCommitsInfo = allCommits.flat().map((element) => {
+      const { sha, url, commit } = element;
+      const { author, message } = commit;
+
+      return {
+        sha,
+        url,
+        author,
+        message,
+      };
+    });
+
+    setCommitList(filterCommitsInfo);
   } catch (error) {
     throw new Error(error);
   }
 };
 
-export default getCommitList;
+const getSingleCommit = async ({ owner, repo, ref }) => {
+  const commitUrl = `${setCommitBaseUrl({ owner, repo })}/${ref}`;
+
+  const response = await fetchWithAuth(commitUrl, DIFF_MEDIA_TYPE);
+  const changedCode = await response.text();
+
+  return getChanges(changedCode);
+};
+
+export { getCommitList, getSingleCommit };
