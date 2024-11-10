836d13a84e3192d871ed7f857db2a1d1cd082519
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2024-11-11 00:22:01 +0900

    fix: 에러 메세지 오타 수정

--- src/shared/constants/index.js
+++ src/shared/constants/index.js
@@ -4,7 +4,7 @@ const ERROR_MESSAGES = {
   invalidURL:
     "적합하지 않은 URL입니다. [https://github.com/소유자이름/레포지토리이름/...] 혹은 [github.com/소유자이름/레포지토리이름/] 처럼 URL를 넣어주세요!",
   invalidGithubURL:
-    "소유자 혹은 레포지토리 이름을 찾을 수 없는 링크입니다. 혹시 레포지토가 private으로 설정 되어있거나 잘못된 링크 인가요?",
+    "소유자 혹은 레포지토리 이름을 찾을 수 없는 링크입니다. 혹시 레포지토리가 private으로 설정 되어있거나 잘못된 링크 인가요?",
   rateLimitExceeded:
     "저희 Github Token 을 모두 소모했습니다! 조금 뒤에 다시 시도 해주세요!",
   networkError: "새로 고침 후 올바른 URL로 다시 시도 해주세요!",
