1e0f3335457ffc6ef1e11a81d5b3f20b81e05227
howyoujini <howyoujini@gmail.com>
2024-11-10 22:53:29 +0900

    feat: CommitQualityScore 를 보여주는 컴포넌트 작성

---
+++ src/features/commit/components/CommitValidationMark.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+
+const CommitQualityScore = ({ qualityScore }) => {
+  return (
+    <div className={`w-20 rounded px-1 py-1 text-xl font-bold text-blue-500`}>
+      Score: {qualityScore}
+    </div>
+  );
+};
+
+CommitQualityScore.propTypes = {
+  qualityScore: PropTypes.number.isRequired,
+};
+
+export default CommitQualityScore;
