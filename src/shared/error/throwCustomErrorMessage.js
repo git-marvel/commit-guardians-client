9e74044e0016b63d25bfd8f4aa6c129238178d80
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2024-11-11 14:58:54 +0900

    refactor: 분기처리 로직을 if에서 swtich로 변경

--- src/shared/error/throwCustomErrorMessage.js
+++ src/shared/error/throwCustomErrorMessage.js
@@ -1,14 +1,15 @@
 import { ERROR_MESSAGES } from "../constants";
 
 const throwCustomErrorMessage = (error) => {
-  if (error.status === 403 || error.status === 429) {
-    throw new Error(ERROR_MESSAGES.rateLimitExceeded);
+  switch (error.status) {
+    case 403:
+    case 429:
+      throw new Error(ERROR_MESSAGES.rateLimitExceeded);
+    case 404:
+      throw new Error(ERROR_MESSAGES.invalidGithubURL);
+    default:
+      throw new Error(ERROR_MESSAGES.networkError);
   }
-  if (error.status === 404) {
-    throw new Error(ERROR_MESSAGES.invalidGithubURL);
-  }
-
-  throw new Error(ERROR_MESSAGES.networkError);
 };
 
 export default throwCustomErrorMessage;
