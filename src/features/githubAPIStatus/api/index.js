521097b8da14829dbb197e5dad9acf27de4c0e90
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2024-11-11 14:42:20 +0900

    refactor: 재사용성을 위해 사용되는 주소를 상수로 처리

--- src/features/githubAPIStatus/api/index.js
+++ src/features/githubAPIStatus/api/index.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
+const GITHUB_STATUS_API_ADRESS =
+  "https://www.githubstatus.com/api/v2/status.json";
+
 const getGithubStatus = async () => {
   try {
-    const githubStatus = await axios.get(
-      "https://www.githubstatus.com/api/v2/status.json"
-    );
+    const githubStatus = await axios.get(GITHUB_STATUS_API_ADRESS);
     return githubStatus.data.status.indicator;
   } catch {
     return "unknown";
