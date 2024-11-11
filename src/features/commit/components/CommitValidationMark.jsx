85da9e8ccd5cb4c0a221b8e12b10831491426b46
howyoujini <howyoujini@gmail.com>
2024-11-11 14:14:59 +0900

    refactor: tailwind 동적 스타일에서 정적 스타일로 변경

--- src/features/commit/components/CommitValidationMark.jsx
+++ src/features/commit/components/CommitValidationMark.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const CommitQualityScore = ({ qualityScore }) => {
   return (
-    <div className={`w-20 rounded px-1 py-1 text-xl font-bold text-blue-500`}>
+    <div className="w-20 rounded px-1 py-1 text-xl font-bold text-blue-500">
       Score: {qualityScore}
     </div>
   );
