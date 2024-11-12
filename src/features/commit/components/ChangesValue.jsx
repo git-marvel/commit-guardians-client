87e5fece31ece00ca0f55c23014fabb56de544df
howyoujini <howyoujini@gmail.com>
2024-11-12 21:18:38 +0900

    feat: changes 막대그래프 구현
    perfect changes / 전체 changes: 이 점수를 바탕으로 가로 막대 그래프를 구현했습니다.

--- src/features/commit/components/ChangesValue.jsx
+++ src/features/commit/components/ChangesValue.jsx
@@ -1,28 +1,38 @@
 import PropTypes from "prop-types";
+import { memo } from "react";
 import FileIcon from "../../../shared/assets/svg/file-icon.svg";
+import BarGraph from "../../../shared/components/BarGraph";
 
-const ChangesValue = ({ commit }) => {
+const ChangesValue = memo(({ commit }) => {
+  if (!commit.numOfFiles) return null;
+
   return (
-    <>
-      {commit.numOfFiles !== null && commit.numOfFiles !== 0 && (
-        <>
-          <img
-            src={FileIcon}
-            alt="File Icon"
-            width="20"
-            height="18"
-            color="slate-400"
-            className="my-0.5"
-          />
-          <p className="pl-1 font-bold text-slate-400">{commit.numOfFiles}</p>
-        </>
-      )}
-    </>
+    <div className="flex">
+      <img
+        src={FileIcon}
+        alt="File Icon"
+        width="20"
+        height="18"
+        color="slate-400"
+        className="my-0.5"
+      />
+      <p className="pl-1 pr-4 font-bold text-slate-400">{commit.numOfFiles}</p>
+      <BarGraph
+        totalChanges={commit.numOfChanges}
+        qualityScore={commit.qualityScore}
+      />
+    </div>
   );
-};
+});
 
 ChangesValue.propTypes = {
-  commit: PropTypes.object.isRequired,
+  commit: PropTypes.shape({
+    numOfFiles: PropTypes.number,
+    numOfChanges: PropTypes.number,
+    qualityScore: PropTypes.number,
+  }).isRequired,
 };
+
+ChangesValue.displayName = "ChangesValue";
 
 export default ChangesValue;
