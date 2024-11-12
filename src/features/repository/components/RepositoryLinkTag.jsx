3a521fbdfdabb8ffe2589190cff3ada16c095b5a
howyoujini <howyoujini@gmail.com>
2024-11-12 17:44:37 +0900

    feat: repository 이름을 전역상태에 저장하여 보여주기

---
+++ src/features/repository/components/RepositoryLinkTag.jsx
@@ -0,0 +1,20 @@
+import useCommitStore from "../../commit/store/useCommitStore";
+
+const RepositoryLinkTag = () => {
+  const repository = useCommitStore((state) => state.repository);
+
+  return (
+    <a
+      href={`https://github.com/${repository.owner}/${repository.repo}`}
+      target="_blank"
+    >
+      <span>{repository.owner}</span>
+      <span className="px-2 text-purple-400">/</span>
+      <span className="text-lg font-semibold text-slate-800">
+        {repository.repo}
+      </span>
+    </a>
+  );
+};
+
+export default RepositoryLinkTag;
