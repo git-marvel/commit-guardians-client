07101688cbbd76bedef5fb90ce5b6ea6139bf5ff
howyoujini <howyoujini@gmail.com>
2024-11-13 12:14:59 +0900

    feat: commit author 가 null 인 경우, default UI 대응

--- src/features/commit/components/AvatarValue.jsx
+++ src/features/commit/components/AvatarValue.jsx
@@ -1,18 +1,17 @@
 import PropTypes from "prop-types";
 
-const AvatarValue = ({ author }) => {
-  if (!author.avatar_url) {
-    return null;
-  }
+const DEFAULT_IMG_URL =
+  "https://github.githubassets.com/assets/mona-loading-default-c3c7aad1282f.gif";
 
+const AvatarValue = ({ author }) => {
   return (
     <>
       <img
-        src={author.avatar_url}
+        src={author.avatar_url ?? DEFAULT_IMG_URL}
         alt="avatar_url"
         className="w-5 rounded-full"
       />
-      <p className="ml-2 text-sm text-slate-700">{author.name}</p>
+      <p className="ml-2 text-sm text-slate-700">{author.name ?? "unknown"}</p>
     </>
   );
 };
