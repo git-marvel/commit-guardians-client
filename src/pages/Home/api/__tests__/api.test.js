b066b716cd5fe09109f34d7091be14a1a82d0f5c
howyoujini <howyoujini@gmail.com>
2024-11-13 20:07:14 +0900

    test: Github repository URL 을 이용하여 커밋 목록을 가져오는 테스트 작성

---
+++ src/pages/Home/api/__tests__/api.test.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { describe, expect, it, vi } from "vitest";
+import { getCommitDiffList, getCommitList } from "../index";
+import diffPerCommit from "./diffPerCommit.mock";
+import mockCommits from "./mockCommits.mock";
+import mockCommitsToCheck from "./mockCommitsToCheck.mock";
+
+vi.mock("axios");
+
+describe("Home API", () => {
+  const mockOwner = "owner";
+  const mockRepo = "repo";
+
+  describe("getCommitList 테스트", () => {
+    it("하나의 레포지토리의 모든 커밋들을 가져올 수 있습니다", async () => {
+      axios.get.mockResolvedValueOnce({
+        data: mockCommits,
+        headers: { link: null },
+      });
+
+      const result = await getCommitList({ owner: mockOwner, repo: mockRepo });
+      expect(result).toEqual(mockCommits);
+    });
+  });
+
+  describe("getCommitDiffList 테스트", () => {
+    it("각 커밋에 따른 diff 를 받을 수 있습니다", async () => {
+      mockCommitsToCheck.forEach(() => {
+        axios.get.mockResolvedValueOnce({
+          data: diffPerCommit,
+        });
+      });
+
+      const diffs = await getCommitDiffList({
+        owner: mockOwner,
+        repo: mockRepo,
+        commitsToCheck: mockCommitsToCheck,
+      });
+
+      expect(diffs).toHaveLength(mockCommitsToCheck.length);
+    });
+  });
+});
