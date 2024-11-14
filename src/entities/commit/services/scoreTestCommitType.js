1d596d0ee4e045b8457b43ec6a0e8e408fbfa720
soohan <evepledge@gmail.com>
2024-11-14 16:11:52 +0900

    refactor: 파일 개수 누적하는 부분 reduce 메소드로 변경

--- src/entities/commit/services/scoreTestCommitType.js
+++ src/entities/commit/services/scoreTestCommitType.js
@@ -1,32 +1,28 @@
 const TEST_TYPE_VALIDATED_KEYWORDS = ["test", "tests", "spec", "mock"];
 
+const isTestFile = (partOfFileName) => {
+  return TEST_TYPE_VALIDATED_KEYWORDS.some((extension) =>
+    partOfFileName.endsWith(extension)
+  );
+};
+
 /**
  * @param {Object} diffObj - 변경사항 객체
  * @returns {number} commitScore - 최종 점수
  */
 const scoreTestCommitType = (diffObj) => {
-  const passedFilesCount = Object.keys(diffObj).filter((filePath) => {
-    const separatedFileNames = filePath
+  const totalFiles = Object.keys(diffObj);
+  const passedFilesCount = totalFiles.reduce((count, filePath) => {
+    const fileName = filePath
       .substring(filePath.lastIndexOf("/") + 1, filePath.lastIndexOf("."))
-      .toLowerCase()
-      .split(".");
+      .toLowerCase();
 
-    for (const name of separatedFileNames) {
-      const isTestType = TEST_TYPE_VALIDATED_KEYWORDS.some((extension) =>
-        name.endsWith(extension)
-      );
+    return isTestFile(fileName) || fileName.split(".").some(isTestFile)
+      ? count + 1
+      : count;
+  }, 0);
 
-      if (isTestType) {
-        return isTestType;
-      }
-    }
-
-    return false;
-  }).length;
-
-  const commitScore = Math.floor(
-    (passedFilesCount / Object.keys(diffObj).length) * 100
-  );
+  const commitScore = Math.floor((passedFilesCount / totalFiles.length) * 100);
 
   return commitScore;
 };
