a9453dd2b0bdafce0f79a4c9618b793063fe27aa
soohan <evepledge@gmail.com>
2024-11-14 23:14:04 +0900

    refactor: docs타입에 대한 로직 최적화

--- src/entities/commit/services/scoreDocsCommitType.js
+++ src/entities/commit/services/scoreDocsCommitType.js
@@ -11,24 +11,28 @@ const DOCS_TYPE_FILENAME_EXTENSIONS = [
   ".pdf",
 ];
 
+const isDocsFile = (filePath) => {
+  return DOCS_TYPE_FILENAME_EXTENSIONS.some((extension) =>
+    filePath.endsWith(extension)
+  );
+};
+
 /**
  * @param {Object} diffObj - 변경사항 객체
  * @returns {number} commitScore - 최종 점수
  */
 const scoreDocsCommitType = (diffObj) => {
-  if (!diffObj || Object.keys(diffObj).length === 0) {
+  const totalFiles = Object.keys(diffObj);
+
+  if (totalFiles.length === 0) {
     return 0;
   }
 
-  const passedFilesCount = Object.keys(diffObj).filter((filePath) => {
-    return DOCS_TYPE_FILENAME_EXTENSIONS.some((extension) =>
-      filePath.endsWith(extension)
-    );
-  }).length;
+  const passedFilesCount = totalFiles.reduce((count, filePath) => {
+    return isDocsFile(filePath) ? count + 1 : count;
+  }, 0);
 
-  const commitScore = Math.floor(
-    (passedFilesCount / Object.keys(diffObj).length) * 100
-  );
+  const commitScore = Math.floor((passedFilesCount / totalFiles.length) * 100);
 
   return commitScore;
 };
