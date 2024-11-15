897d279ff7799aa9e096ff948688fa88c01135ca
howyoujini <howyoujini@gmail.com>
2024-11-15 19:47:47 +0900

    feat: github API status 버튼 클릭시, 해당 status를 볼 수 있는 링크로 이동

--- src/features/githubAPIStatus/components/GithubAPIStatus.jsx
+++ src/features/githubAPIStatus/components/GithubAPIStatus.jsx
@@ -1,15 +1,19 @@
 import useGithubAPIStatus from "../hooks/useGithubAPIStatus";
 
+const GITHUB_STATUS_ADDRESS = "https://www.githubstatus.com";
+
 const GithubAPIStatus = () => {
   const { githubAPIStatusColor } = useGithubAPIStatus();
 
   return (
-    <div className="flex cursor-pointer items-center rounded-full border bg-slate-50 px-4 py-1">
-      <p className="pr-2 text-sm text-slate-400">GitHub API Status</p>
-      <div
-        className={`h-4 w-4 rounded-full border ${githubAPIStatusColor}`}
-      ></div>
-    </div>
+    <a href={GITHUB_STATUS_ADDRESS} target="_blank">
+      <div className="flex items-center rounded-full border bg-slate-50 px-4 py-1">
+        <p className="pr-2 text-sm text-slate-400">GitHub API Status</p>
+        <div
+          className={`h-4 w-4 rounded-full border ${githubAPIStatusColor}`}
+        ></div>
+      </div>
+    </a>
   );
 };
 
