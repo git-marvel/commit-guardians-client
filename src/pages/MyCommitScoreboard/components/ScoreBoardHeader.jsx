fce9b6cd1d041479984cb504b623258fa9ef4093
soohan <evepledge@gmail.com>
2024-11-17 19:34:37 +0900

    feat: ScoreBoard 페이지에 추가 정보 표시 및 DATE의 GMT 시간 제거

--- src/pages/MyCommitScoreboard/components/ScoreBoardHeader.jsx
+++ src/pages/MyCommitScoreboard/components/ScoreBoardHeader.jsx
@@ -1,4 +1,7 @@
 import PropTypes from "prop-types";
+import InfoValue from "../../../features/commit/components/InfoValue";
+
+const time = new Date().toTimeString().substring(8);
 
 const ScoreBoardHeader = ({ gridCols }) => {
   return (
@@ -7,10 +10,16 @@ const ScoreBoardHeader = ({ gridCols }) => {
     >
       <p className="col-span-1 px-2 py-1">TYPE</p>
       <p className="col-span-6 px-2 py-1">COMMIT MESSAGE</p>
-      <p className="col-span-3 px-2 py-1">CHANGES</p>
+      <p className="relative col-span-3 px-2 py-1">
+        CHANGES
+        <InfoValue text="코드 수정에서 제거와 추가가 짝을 이루는 변경 단위" />
+      </p>
       <p className="col-span-1 px-2 py-1">SCORE</p>
       <p className="col-span-2 px-2 py-1">AUTHOR</p>
-      <p className="col-span-2 px-2 py-1">DATE</p>
+      <p className="relative col-span-2 px-2 py-1">
+        DATE
+        <InfoValue text={time} />
+      </p>
       <p className="col-span-1 px-2 py-1">SHA</p>
     </div>
   );
