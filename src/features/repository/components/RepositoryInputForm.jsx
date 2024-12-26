39bb981d82e431515bb93bbc03b148d597ddc0e2
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2024-12-26 19:22:24 +0900

    feat: github 로그인 구현

--- src/features/repository/components/RepositoryInputForm.jsx
+++ src/features/repository/components/RepositoryInputForm.jsx
@@ -2,13 +2,24 @@ import PropTypes from "prop-types";
 import Button from "../../../shared/components/Button";
 import CheckIcon from "../../../shared/components/CheckIcon";
 import GithubIcon from "../../../shared/components/GithubIcon";
+import usePersistentStore from "../../../shared/store/usePersistentStore";
 
 function RepositoryInputForm({ onSubmit }) {
+  const { githubToken } = usePersistentStore();
+
+  const GITHUB_CLIENT_ID = import.meta.env.VITE_GITHUB_CLIENT_ID;
+  const REDIRECT_URI = import.meta.env.VITE_GITHUB_REDIRECT_URI;
+
+  const handleLogin = () => {
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=user,repo`;
+    window.location.href = githubAuthUrl;
+  };
+
   return (
     <form
       className="mx-auto w-full max-w-2xl"
       method="post"
-      onSubmit={onSubmit}
+      onSubmit={githubToken && onSubmit}
     >
       <label className="mb-2 block text-base font-medium text-gray-900 dark:text-white">
         Github Repository URL
@@ -20,18 +31,22 @@ function RepositoryInputForm({ onSubmit }) {
         <input
           type="url"
           name="repositoryURL"
-          required
+          required={githubToken ? true : false}
           placeholder="ex) https://github.com/git-marvel/commit-guardians-client"
           className="block w-full min-w-0 flex-1 rounded-none rounded-e-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-sky-500 focus:ring-sky-500 dark:border-gray-300 dark:bg-gray-700 dark:text-white dark:placeholder-slate-400 dark:focus:border-sky-500 dark:focus:ring-sky-500"
         />
       </div>
       <div className="flex justify-center">
-        <Button>
-          <>
-            <CheckIcon />
-            <span>Check the Quality</span>
-          </>
-        </Button>
+        {githubToken ? (
+          <Button>
+            <>
+              <CheckIcon />
+              <span>Check the Quality</span>
+            </>
+          </Button>
+        ) : (
+          <Button onClick={handleLogin}>GitHub으로 로그인</Button>
+        )}
       </div>
     </form>
   );
