f59b330b8edb99b98eb04bca68a55955bff7ec4e
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2024-12-26 23:01:51 +0900

    feat: 비로그인시 제공된 예시만 확인할 수 있는 선택박스 구현

--- src/features/repository/components/RepositoryInputForm.jsx
+++ src/features/repository/components/RepositoryInputForm.jsx
@@ -19,7 +19,7 @@ function RepositoryInputForm({ onSubmit }) {
     <form
       className="mx-auto w-full max-w-2xl"
       method="post"
-      onSubmit={githubToken && onSubmit}
+      onSubmit={onSubmit}
     >
       <label className="mb-2 block text-base font-medium text-gray-900 dark:text-white">
         Github Repository URL
@@ -28,13 +28,29 @@ function RepositoryInputForm({ onSubmit }) {
         <span className="inline-flex items-center rounded-s-md border border-e-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-300 dark:bg-gray-600 dark:text-gray-400">
           <GithubIcon />
         </span>
-        <input
-          type="url"
-          name="repositoryURL"
-          required={githubToken ? true : false}
-          placeholder="ex) https://github.com/git-marvel/commit-guardians-client"
-          className="block w-full min-w-0 flex-1 rounded-none rounded-e-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-sky-500 focus:ring-sky-500 dark:border-gray-300 dark:bg-gray-700 dark:text-white dark:placeholder-slate-400 dark:focus:border-sky-500 dark:focus:ring-sky-500"
-        />
+        {githubToken ? (
+          <input
+            type="url"
+            name="repositoryURL"
+            required
+            placeholder="ex) https://github.com/git-marvel/commit-guardians-client"
+            className="block w-full min-w-0 flex-1 rounded-none rounded-e-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-sky-500 focus:ring-sky-500 dark:border-gray-300 dark:bg-gray-700 dark:text-white dark:placeholder-slate-400 dark:focus:border-sky-500 dark:focus:ring-sky-500"
+          />
+        ) : (
+          <select
+            type="url"
+            name="repositoryURL"
+            className="block w-full min-w-0 flex-1 rounded-none rounded-e-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-sky-500 focus:ring-sky-500 dark:border-gray-300 dark:bg-gray-700 dark:text-white dark:placeholder-slate-400 dark:focus:border-sky-500 dark:focus:ring-sky-500"
+          >
+            <option value="https://github.com/facebook/react">
+              facebook/react
+            </option>
+            <option value="https://github.com/vuejs/vue">vuejs/vue</option>
+            <option value="https://github.com/pmndrs/zustand">
+              pmndrs/zustand
+            </option>
+          </select>
+        )}
       </div>
       <div className="flex justify-center">
         {githubToken ? (
